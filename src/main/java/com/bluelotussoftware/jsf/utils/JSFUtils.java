a9a8de096a20c9efccd2f3c4d216cc99f4e13dec
John Yeary <jyeary@bluelotussoftware.com>
2013-01-06 00:36:46 -0500

    Updated comments, ID, and copyright.

--- src/main/java/com/bluelotussoftware/jsf/utils/JSFUtils.java
+++ src/main/java/com/bluelotussoftware/jsf/utils/JSFUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 Blue Lotus Software, LLC>.
+ * Copyright 2012-2013 Blue Lotus Software, LLC.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/*
+ * $Id$
+ */
 package com.bluelotussoftware.jsf.utils;
 
 import java.io.Serializable;
@@ -22,6 +25,7 @@ import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 
 /**
+ * A series of utility methods to make JSF development easier.
  *
  * @author John Yeary <jyeary@bluelotussoftware.com>
  * @version 1.0
